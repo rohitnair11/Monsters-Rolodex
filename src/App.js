a4bfb501146fb0b1137fc0ea7406f94459151d11
Rohit Nair <nair1111998@gmail.com>
2020-06-25 20:24:42 -0400

    Add deploy files

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ componentDidMount(){
 
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox placeholder="Search Monsters" handleChange={e => this.setState({ searchField: e.target.value})} />
         <CardList monsters={filteredMonsters}>
         
